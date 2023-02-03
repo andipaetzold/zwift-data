0961449f2b8d00084f41db643d592010b5dcfb01
Andi Pätzold <github@andipaetzold.com>
2023-02-03 21:18:48 +0100

    fix: adjust scotland bounds

--- src/worlds.ts
+++ src/worlds.ts
@@ -116,8 +116,8 @@ export const worlds: ReadonlyArray<World> = [
     name: "Scotland",
     slug: "scotland",
     bounds: [
-      [55.67595, -5.17753],
-      [55.6185, -5.28053],
+      [55.67595, -5.28053],
+      [55.6185, -5.17753],
     ],
     imageUrl: "https://cdn.zwift.com/static/images/maps/MiniMap_Scotland.png",
   },
